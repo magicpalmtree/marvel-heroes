a88d9dced982592784c8598800de0792241ecee6
magicpalmtree <magicpalmtree.space@gmail.com>
2017-06-01 07:04:30 +0800

    updating server process

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.use('/', function (req, res) {
 
 });
 
-app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT);
 
 // app.listen(app.get('port'), function() {
 //   console.log('listening on port 3000')
