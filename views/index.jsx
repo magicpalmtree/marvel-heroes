91d08dd21d8b50b3bc7371d83cb1436870033d07
magicpalmtree <magicpalmtree.space@gmail.com>
2017-06-01 08:58:29 +0800

    updating enviroment to development

--- views/index.jsx
+++ views/index.jsx
@@ -201,7 +201,6 @@ class Body extends Component{
 class Characters extends Component{
   constructor(props){
     super(props)
-    console.log(this.props)
     this.state = {
       mySelected : false,
       mySelectId : '',
